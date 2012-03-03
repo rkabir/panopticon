732025a87f6e8206305af4e4b7729c8c1d65cb98
Ryan Kabir <rkabir@alum.mit.edu>
2012-03-03 17:30:03 -0400

    put files in date-based folders

--- screenshot.rb
+++ screenshot.rb
@@ -3,7 +3,16 @@
 dt = Time.now
 path = "/Users/rkabir/panopticon/"
 external = "external/"
-filename1 = path + "#{dt.year}_#{dt.mon}_#{dt.day}_#{dt.hour}_#{dt.min}_#{dt.sec}.jpg"
-filename2 = path + external + "#{dt.year}_#{dt.mon}_#{dt.day}_#{dt.hour}_#{dt.min}_#{dt.sec}_2.jpg"
+year = dt.year.to_s
+month = dt.month.to_s.length == 1 ? "0#{dt.month}" : dt.month.to_s
+day = dt.day.to_s.length == 1 ? "0#{dt.day}" : dt.day.to_s
+
+date_path = "#{year}/#{month}/#{day}/"
+path = path + date_path
+
+%x[ mkdir -p #{path}]
+
+filename1 = path + "#{year}_#{month}_#{day}_#{dt.hour}_#{dt.min}_#{dt.sec}.jpg"
+filename2 = path + external + "#{year}_#{month}_#{day}_#{dt.hour}_#{dt.min}_#{dt.sec}_2.jpg"
 
 %x[ /usr/sbin/screencapture -tjpg -x #{filename1} #{filename2}]
